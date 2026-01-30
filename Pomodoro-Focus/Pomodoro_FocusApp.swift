3a55d9fc5fed0ecbe4fac274357bc5ec0d3c2d2e
Patrick.Lanham <p.lanham91@icloud.com>
2026-01-30 19:49:22 +0100

    Initial Commit

---
+++ Pomodoro-Focus/Pomodoro_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_FocusApp.swift
+//  Pomodoro-Focus
+//
+//  Created by Patrick Lanham on 30.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
